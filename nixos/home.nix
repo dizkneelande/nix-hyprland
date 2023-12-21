1036fda043d1e75b12cca87e1a2856dcbdaa5cff
dizkneelande <105323825+dizkneelande@users.noreply.github.com>
2023-12-21 22:43:09 +1000

    Update home.nix

--- nixos/home.nix
+++ nixos/home.nix
@@ -1,13 +1,6 @@
 { config, pkgs, ... }:
 
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/release-23.11.tar.gz";
-in
 {
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
-
   home-manager.users.nell = {
 
   home.username = "nell";
